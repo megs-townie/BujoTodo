26b5090ed1898cc6dbfdab38b9de6d8b25b6bcb4
Meagan Townsend <meagantownsend337@gmail.com>
2023-10-20 17:16:11 -0300

    Working calculator with layout changes

--- M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.accessibilityservice.AccessibilityGestureEvent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,38 +14,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Action when "Add" button is pressed
-        Button button = (Button) findViewById(R.id.b_Add);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Action for each button
+        Button btnAdd = findViewById(R.id.b_Add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("M01_Calculator ADD BUTTON", "User tapped the Add button");
-
-                Double d1 = 0.0;
-                Double d2 = 0.0;
-                Double answer = 0.0;
-
-                EditText textN1 = (EditText) findViewById(R.id.editTextN1);
-                EditText textN2 = (EditText) findViewById(R.id.editTextN2);
-                // we actually don't need to get ans from screen
-                EditText textANS = (EditText) findViewById(R.id.editTextNumAns);
-
-                try {
-                    d1 = Double.parseDouble(textN1.getText().toString());
-                    d2 = Double.parseDouble(textN2.getText().toString());
-                    answer = d1 + d2;
-                }
-                catch (Exception e) {
-                    Log.w("M01_Calculator ADD BUTTON", "Add Selected with no inputs ... " + answer);
-                }
-
-                // Set the Answer into the the answer field
-                textANS.setText(answer.toString());
-
-                // log what we are doing
-                Log.w("M01_Calculator ADD BUTTON", "Add Selected with => " + d1 + " + " + d2 + "=" + answer);
+                performCalculation('+');
             }
         });
 
+        Button btnSubtract = findViewById(R.id.b_Subtract);
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                performCalculation('-');
+            }
+        });
+
+        Button btnMultiply = findViewById(R.id.b_Multiply);
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                performCalculation('*');
+            }
+        });
+
+        Button btnDivide = findViewById(R.id.b_Divide);
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                performCalculation('/');
+            }
+        });
     }
 
+    private void performCalculation(char operation) {
+        Double d1 = 0.0;
+        Double d2 = 0.0;
+        Double answer = 0.0;
+
+        EditText textN1 = findViewById(R.id.editTextN1);
+        EditText textN2 = findViewById(R.id.editTextN2);
+        EditText textANS = findViewById(R.id.editTextNumAns);
+
+        try {
+            d1 = Double.parseDouble(textN1.getText().toString());
+            d2 = Double.parseDouble(textN2.getText().toString());
+            switch (operation) {
+                case '+': answer = d1 + d2; break;
+                case '-': answer = d1 - d2; break;
+                case '*': answer = d1 * d2; break;
+                case '/':
+                    if (d2 != 0) {
+                        answer = d1 / d2;
+                    } else {
+                        Log.w("M01_Calculator", "Attempted division by zero");
+                        return;
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            Log.w("M01_Calculator", "Operation selected with no valid inputs ... " + answer);
+            return;
+        }
+
+        // Set the answer into the answer field
+        textANS.setText(answer.toString());
+
+        // Log the operation
+        Log.w("M01_Calculator", operation + " Selected with => " + d1 + " " + operation + " " + d2 + "=" + answer);
+    }
 }
